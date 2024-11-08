cee1e87a3f7e87f0ce1d99b5fea348a8b9470ab9
Yzma01 <marchena.isma@gmail.com>
2024-11-07 23:33:21 -0600

    puta

--- commit.bat
+++ commit.bat
@@ -1,3 +1,11 @@
+:: Añadir todos los cambios
 git add .
-git commit -m "commit for pull"
+
+:: Solicitar mensaje de commit
+set /p commitMsg="Ingresa el mensaje de commit: "
+
+:: Ejecutar commit con el mensaje proporcionado
+git commit -m "%commitMsg%"
+
+:: Hacer push al branch especificado
 git push origin yzma
